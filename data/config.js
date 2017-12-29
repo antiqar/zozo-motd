36e179547f6b69cf7c3c3d2861a86800f58649be
antiqar <antiqar@users.noreply.github.com>
2017-12-29 21:58:24 +0500

    JS config placed out of index.html, all advertisement images moved to separate folder, added image new-year-2018.jpg, new advertisement about New Year 2018.

---
+++ data/config.js
@@ -0,0 +1,83 @@
+var config = {
+	lang : 'ru',
+	face_max : 2,
+	adv_img : 'data/img/adv/new-year-2018.jpg',
+	adv_url : 'https://zozo.gg/?p=12046',
+	adv_end : '2018-01-08 15:00:00 GMT',
+	translation : {
+		ru : {
+			server : 'Сервер',
+			help : 'Помощь',
+			discuss : 'Обсуждение',
+			bans : 'Баны',
+			report : 'Жалобы',
+			stats : 'Статистика',
+			forum : 'Форум',
+			chat : 'Чат',
+			all_servers : 'Все серверы',
+			vip_btn : 'Получи Бонусы',
+			vip_intro : '<strong>VIP-бонусы</strong> дают вам некоторые преимущества и привилегии во время игры.<br>Благодаря вашим пожертвованиям серверы будут развиваться и улучшаться.<br><br>Спасибо за поддержку!',
+			vip_list : {
+				l4d2 : [
+					'Огнестрельное и рукопашное оружие.',
+					'Таблетки, Адреналин, Лазерный прицел, Бомба.',
+					'Добавочное здоровье за Выживших и Зараженных.',
+					'Резервный слот для подключения на полный сервер.',
+					'Приветствие в момент подключения к серверу.',
+					'Вас не отключит из-за большого пинга.',
+					'Моделька персонажа и цвет модельки.',
+					'Тег [VIP] перед ником в чате игры.',
+					'И многое другое...'
+				],
+				tf : [
+					'Более 50 необычных эффектов над головой.',
+					'Резервный слот для подключения на полный сервер.',
+					'Мгновенное возрождение и вид от третьего лица.',
+					'Добавочные здоровье, патроны и скорость бега.',
+					'Вас не смогут выгнать через голосование.',
+					'Вас не отключит из-за большого пинга.',
+					'Нет урона от падения с высоты.',
+					'Бессмертие в конце раунда.',
+					'И многое другое...'
+				]
+			}
+		},
+		en : {
+			server : 'Server',
+			help : 'Help',
+			discuss : 'Discussion',
+			bans : 'Bans',
+			report : 'Report Player',
+			stats : 'Statistics',
+			forum : 'Forum',
+			chat : 'Chat',
+			all_servers : 'All Servers',
+			vip_btn : 'Become VIP',
+			vip_intro : '<strong>VIP status</strong> gives you some of the benefits and privileges during the game.<br>Because of your donations our servers will be improving.<br><br>Thank you for your support!',
+			vip_list : {
+				l4d2 : [
+					'Guns and melee weapons.',
+					'Survivor\'s character and color.',
+					'Pills, Adrenaline, Laser Sight, Pipe-Bomb.',
+					'Additional health for Survivors and Infected.',
+					'Reserved slot to connect to a full server.',
+					'[VIP] tag in chat before the nickname.',
+					'Welcome message when connected.',
+					'High-ping protection.',
+					'And much more...'
+				],
+				tf : [
+					'More than 50 unusual effects above your head.',
+					'Instant respawn and switchable third-person view.',
+					'Additional health, ammo and movement speed.',
+					'Reserved slot to connect to a full server.',
+					'Immortality at the end of round.',
+					'High-ping protection.',
+					'Vote kick immunity.',
+					'No fall damage.',
+					'And much more...'
+				]
+			}
+		}
+	}
+}
